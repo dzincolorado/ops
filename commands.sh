e9e0eabed69cc9b8b6ee6079a36c2ebe3521863f
Denis Ziegler <dzincolorado@yahoo.com>
2014-04-17 16:11:16 -0400

    add meteor path to .bashrc

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
-kill all processes that are found with <search string>
+#kill all processes that are found with <search string>
 ps aux | grep -ie <search string> | awk '{print $2}' | xargs kill -9 
+
+#add variables to path globally for bash:
+echo 'export PATH=$PATH:~/.meteor' >> ~/.bashrc
